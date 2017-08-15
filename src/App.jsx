a7b386a7dc56ca487d9a196441d976f81fd964a7
Jenna Zenk <zenkjenna@gmail.com>
2017-08-15 09:29:31 +0200

    web3 integration + test a function from melon.js

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,5 @@
-import React, { Component } from "react";
-import {
-  BrowserRouter,
-  Route,
-  Link,
-  HashRouter,
-  Switch,
-} from "react-router-dom";
+import React from "react";
+import { Route, Link, HashRouter, Switch } from "react-router-dom";
 import GetStarted from "./components/newUser/getStarted";
 import ManagerView from "./components/existingUser/managerView";
 
@@ -24,13 +18,8 @@ const App = () =>
         <Link to="/manager">MANAGER VIEW</Link>
       </div>
       <hr />
-
-      {/* <HashRouter basename="/" /> */}
       <Switch>
         <Route path="/getstarted" component={GetStarted} />
-        {/* <Route path="/setup" component={Setup} />
-        <Route path="/firstinvest" component={FirstInvest} />
-        <Route path="/firsttrade" component={FirstTrade} /> */}
         <Route path="/manager" component={ManagerView} />
       </Switch>
     </div>
--- src/index.js
+++ src/index.js
@@ -1,9 +1,34 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import Web3 from "web3";
+import { setup, getConfig, createVault } from "@melonproject/melon.js";
 import "./index.css";
 import App from "./App";
 import registerServiceWorker from "./registerServiceWorker";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+window.addEventListener("load", () => {
+  // Checking if Web3 has been injected by the browser (Mist/MetaMask)
+  if (typeof web3 !== "undefined") {
+    // Use Mist/MetaMask's provider
+    window.web3 = new Web3(window.web3.currentProvider);
+    console.log("Found injected web3");
+  } else {
+    console.log("No web3? You should consider trying MetaMask!");
+    // fallback - use your fallback strategy (local node / hosted node + in-dapp id mgmt / fail)
+    window.web3 = new Web3(
+      new Web3.providers.HttpProvider("http://localhost:8545"),
+    );
+  }
+  setup.init({
+    web3: window.web3,
+    daemonAddress: "0x00360d2b7D240Ec0643B6D819ba81A09e40E5bCd",
+  });
+  getConfig().then(res => {
+    console.log("melon.js config ", res);
+  });
+  window.createVault = createVault;
+  // Now you can start your app & access web3 freely:
+  ReactDOM.render(<App />, document.getElementById("root"));
+});
 
 registerServiceWorker();
